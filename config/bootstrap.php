d30f48dfea8cde1b71096a8d7410090071df81ef
Josey Morton <me@joseymorton.com>
2012-05-10 18:21:46 -0500

    removing necessary config file

--- config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	use lithium\core\Libraries;
-
-	// Define path to plugin and other constants
-	defined('COMPOSER_PATH') OR define('COMPOSER_PATH', dirname(__DIR__) . "/vendors/composer/src/");
-
-?>

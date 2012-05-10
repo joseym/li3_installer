a632f791763ea1c83ee8e6d83b24eadfe0edcc20
Josey Morton <me@joseymorton.com>
2012-05-10 12:12:42 -0500

    installing to different path

--- Composer/Installer.php
+++ Composer/Installer.php
@@ -13,7 +13,8 @@ class Installer extends LibraryInstaller
     public function getInstallPath(PackageInterface $package)
     {
         $prefix = substr($package->getPrettyName(), 0, 23);
-        return substr($package->getPrettyName(), 23);
+        $path = explode('/', $package->getPrettyName());
+        return $path[1];
     }
 
     /**
